6b64eae208eb66364b8332cf0dff23e87c2856e9
luokw <luokw@tpl.cntaiping.com>
2017-09-01 16:48:09 +0800

    deving

--- bin/vug-init.js
+++ bin/vug-init.js
@@ -13,7 +13,7 @@ var logger = require('../lib/util/logger')
 var generate = require('../lib/generate')
 //var checkVersion = require('../lib/check-version')
 
-var localPath = require('../lib/util/local-path')
+var localPath = require('../lib/local-path')
 
 var isLocalPath = localPath.isLocalPath
 var getTemplatePath = localPath.getTemplatePath
